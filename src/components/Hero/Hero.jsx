2c83787dd275418f3efff810d915bfce6cb468bf
MaksymChukhrai <m.chukhrai@gmail.com>
2025-03-04 12:54:58 +0200

    -=-

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,33 +1,45 @@
-import React from 'react';
-import './Hero.scss';
+import React, { useState } from 'react';
 import backgroundVideo from '../../assets/videos/Lines_1920x1080_30Fps.webm';
-import profilePhoto from '../../assets/images/me-mob.png';
+import profilePhoto from '../../assets/images/me-mob.webp';
+import spinner from '../../assets/images/loading.gif'; // Спиннер
 
 const Hero = () => {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
   return (
     <section className="hero" id="hero">
       <div className="video-background">
-        <video autoPlay loop muted>
+        {!isVideoLoaded && (
+          <div className="placeholder-image">
+            <img src={spinner} alt="Loading..." className="spinner" />
+          </div>
+        )}
+        <video 
+          autoPlay 
+          loop 
+          muted 
+          onLoadedData={() => setIsVideoLoaded(true)}
+          className={isVideoLoaded ? "video-visible" : "video-hidden"}
+        >
           <source src={backgroundVideo} type="video/webm" />
         </video>
       </div>
       <div className="hero-container">
         <div className="hero-content">
           <div className="profile-image">
-            <img src={profilePhoto} alt="Maksym Chukhrai" />
+            <img src={profilePhoto} alt="Maksym Chukhrai" loading="lazy" />
           </div>
           <div className="hero-text">
             <h1>I'm <span>Maksym Chukhrai</span></h1>
             <h2>Full Stack Developer</h2>
-            <button type="button" className="contact-btn" >
-            <a 
-              href="https://wa.me/yourphonenumber" 
-              
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              Contact Me
-            </a>
+            <button type="button" className="contact-btn">
+              <a 
+                href="https://wa.me/yourphonenumber" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                Contact Me
+              </a>
             </button>
           </div>
         </div>
@@ -37,3 +49,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
